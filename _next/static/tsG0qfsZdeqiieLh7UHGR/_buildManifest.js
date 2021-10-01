66dd08a41d8b7b72d61929066a9275b095f9b87b
vongdarakia <akia@gomerits.com>
2021-10-01 09:05:36 +0000

    Deploying to gh-pages from @ vongdarakia/test-nextjs@44e50ac6317f13f798d03b9bc75056fc192f9a53 🚀

---
+++ _next/static/tsG0qfsZdeqiieLh7UHGR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/63876c1dd666ae620ee1.css","static/chunks/pages/index-f2bd4c8c73486d015d45.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
