75e2d5f1d734a38c49f3fcf54b62d3beb14d4d8d
Akia Vongdara <vongdarakia@gmail.com>
2021-10-04 02:15:35 -0700

    Add NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/dist/client/router";
+import styled from "styled-components";
+import { MenuItem, NavLink } from "./NavLink";
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 30px;
+    border: 1px solid black;
+`;
+
+type Props = {
+    menuItems?: MenuItem[];
+};
+
+export const NavBar = ({ menuItems = [] }: Props) => {
+    const router = useRouter();
+
+    return (
+        <Nav>
+            {menuItems.map((menuItem) => {
+                const isSelected = router.pathname === menuItem.path;
+
+                return <NavLink key={menuItem.name} menuItem={menuItem} isSelected={isSelected} />;
+            })}
+        </Nav>
+    );
+};
